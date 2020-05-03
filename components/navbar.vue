787c90ba164c935c0c6e7cf7bef8a6d6f523e055
ivobug <ivo.bagaric.2@gmail.com>
2020-05-03 11:59:25 +0200

    initialize

---
+++ components/navbar.vue
@@ -0,0 +1,21 @@
+<template>
+
+  <nav class="nav">
+      <div class="logo">
+          <nuxt-link :to="{name:'index'}">
+              Nuxt Fundamentals
+          </nuxt-link>
+      </div>
+  </nav>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <navbar></navbar>
+    <nav>
+      ...
+    </nav>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import navbar from '../components/navbar'
+export default {
+  components:{
+    navbar
+  }
+}
+</script>
+
+<style scoped>
+nav{
+  color: aliceblue;
+}
+
+</style>
